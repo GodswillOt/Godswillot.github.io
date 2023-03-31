a01ea770d17f193f73328f53325f79f9a8cba09d
GodswillOt <efayemiodoh@gmail.com>
2023-03-31 19:50:07 +0100

    Developing my page, to final appearance.

---
+++ javascript/navbar_code.js
@@ -0,0 +1,8 @@
+
+
+const toggleButton = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+toggleButton.addEventListener('click', () => {
+  navbarMenu.classList.toggle('show');
+});
